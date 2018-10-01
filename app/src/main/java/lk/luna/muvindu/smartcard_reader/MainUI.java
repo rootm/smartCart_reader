629a9ff3397ebd7c45e90889bbdd49c05d6bd182
rootm <muvindu.t@gmail.com>
2018-10-01 07:53:03 +0530

    NFC support added

--- app/src/main/java/lk/luna/muvindu/smartcard_reader/MainUI.java
+++ app/src/main/java/lk/luna/muvindu/smartcard_reader/MainUI.java
@@ -1,13 +1,27 @@
 package lk.luna.muvindu.smartcard_reader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainUI extends AppCompatActivity {
+import be.appfoundry.nfclibrary.activities.NfcActivity;
+
+public class MainUI extends NfcActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ui);
     }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        for (String message : getNfcMessages()){
+            Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
