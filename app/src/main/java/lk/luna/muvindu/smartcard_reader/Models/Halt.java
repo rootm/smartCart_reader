a7273e124fc9d67706f4812df8d939e75e80ce56
rootm <muvindu.t@gmail.com>
2018-10-04 10:18:32 +0530

    Halt added and route class haltlist added

---
+++ app/src/main/java/lk/luna/muvindu/smartcard_reader/Models/Halt.java
@@ -0,0 +1,36 @@
+package lk.luna.muvindu.smartcard_reader.Models;
+
+public class Halt {
+    private String name;
+    private String location;
+    private int index;
+
+
+    /**
+     * Getters and Setter for Atriibutes
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
--- app/src/main/java/lk/luna/muvindu/smartcard_reader/Models/Route.java
+++ app/src/main/java/lk/luna/muvindu/smartcard_reader/Models/Route.java
@@ -1,13 +1,18 @@
 package lk.luna.muvindu.smartcard_reader.Models;
 
+import java.util.ArrayList;
+
 public class Route {
     private int routeId;
     private String routeName;
+    private ArrayList<Halt> haltList=new ArrayList<>();
     private double cost;
 
     public void newJourney(String cardNumber){}
     public void finishJourney(String cardNumber){}
     public void addToList(Journey journey){}
+
+
 
     /**
      * Getters and setters for the attributes
@@ -36,5 +41,12 @@ public class Route {
 
     public void setCost(double cost) {
         this.cost = cost;
+    }
+    public ArrayList<Halt> getHaltList() {
+        return haltList;
+    }
+
+    public void setHaltList(ArrayList<Halt> haltList) {
+        this.haltList = haltList;
     }
 }
